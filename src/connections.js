c1a30b7cf8d3df0892ce98430b778488759727dd
Sergio Augusto Moreira Bastos Junior <89741627+sergiofisio@users.noreply.github.com>
2023-03-28 20:53:23 -0300

    Update connections.js

--- src/connections.js
+++ src/connections.js
@@ -1,11 +1,13 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'https://qr-code-generation-ot5kck2bb-novaisbrunoo.vercel.app',
-        port: 5432,
-        user: 'postgres',
-        password: '123456',
-        database: 'users_registers'
+    host: "ec2-34-193-110-25.compute-1.amazonaws.com",
+    port: 5432,
+    user: "zoixghfjcnfqaw",
+    password:
+      "22ff00cf7c89047c44f695d94fa0063ee99f94a13ca0ab7b0bd34392e5507803",
+    database: "ddgck2pq4l8577",
+    ssl: { rejectUnauthorized: false },
     }
 });
 
