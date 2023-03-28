f5191dcc1295741ef81bb2ba2dd5ac89cbb2e573
Sergio Augusto Moreira Bastos Junior <89741627+sergiofisio@users.noreply.github.com>
2023-03-28 20:49:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes');
-
 const app = express();
-
-app.use(express.json());
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use(routes);
 
-app.listen(6000);
+app.listen(process.env.PORT, () => {
+    console.log(`servidor na porta ${process.env.PORT}`)
+});
